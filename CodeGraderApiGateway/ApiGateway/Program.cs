c4bd91c26b1d1e9ba1dffba9639cd77571a241c8
TrDHuy21 <vlsvhuy@gmail.com>
2025-08-21 09:32:17 +0700

    base project

---
+++ CodeGraderApiGateway/ApiGateway/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace ApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            // Add Ocelot
+            builder.Configuration
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddOcelot(); // single ocelot.json file in read-only mode
+
+            builder.Services
+                .AddOcelot(builder.Configuration);
+
+            // Add CORS để frontend có thể gọi API
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowFrontend", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("AllowFrontend");
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.UseOcelot().Wait();
+
+            app.Run();
+        }
+    }
+}
